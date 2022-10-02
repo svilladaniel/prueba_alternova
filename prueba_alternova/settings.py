b3fdd38661d8044bd66b4edb04a88dc012f18edb
daniel <svilladaniel@gmail.com>
2022-10-02 17:16:35 -0500

    database settings

--- prueba_alternova/settings.py
+++ prueba_alternova/settings.py
@@ -75,8 +75,17 @@ WSGI_APPLICATION = 'prueba_alternova.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.postgresql',
+        'NAME' : 'postgres',
+        'HOST' : 'db.usyekndizppjqxaoekkv.supabase.co',
+        'PASSWORD': 'Facilit0$123',
+        # 'HOST' : os.environ.get('SUPABASE_HOST'),
+        # 'PASSWORD': os.environ.get('SUPABASE_PW'),
+        'PORT': 5432,
+        'USER': 'postgres',
+        'CERT' : 'config.prod-ca-2021.crt', 
     }
 }
 
