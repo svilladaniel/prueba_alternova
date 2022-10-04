dff064225292526caec68f788d7b8fd713dcb803
daniel <svilladaniel@gmail.com>
2022-10-04 10:47:28 -0500

    django secret key config

--- prueba_alternova/settings.py
+++ prueba_alternova/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5$e^5^4h#%sovjbw7)7rt)@(oyfo-kc=0jm2jdn5@)ssp+-1-i'
+SECRET_KEY = os.environ.get('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
